1cac01819ca224299407c96f5549cad2194a2e28
HJ <hjk@gmail.com>
2020-09-26 23:09:59 +0900

    MODIFY mixBoard function, ADD printBoard function

--- Bingo.cpp
+++ Bingo.cpp
@@ -5,12 +5,28 @@
 
 using namespace std;
 
+void printBoard(int A[][5]) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			printf("%3d", A[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n\n");
+}
+
 void mixBoard(int A[][5]) {
 	int temp;
 	srand(time(NULL));
 
 	for (int i = 0; i < 100; i++) {
-		SWAP(A[rand() % 25][rand() % 25], A[rand() % 25][rand() % 25], temp);
+		int x1, y1, x2, y2;
+		x1 = rand() % 5;
+		y1 = rand() % 5;
+		x2 = rand() % 5;
+		y2 = rand() % 5;
+
+		SWAP(A[y1][x1], A[y2][x2], temp);
 	}
 }
 
@@ -28,7 +44,10 @@ void makeBoard(int A[][5]) {
 }
 
 int main() {
-
+	int Board[5][5];
+	
+	makeBoard(Board);
+	printBoard(Board);
 
 	return 0;
 }
