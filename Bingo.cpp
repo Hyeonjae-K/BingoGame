56d4839eea3c7e433f20184dd385f6e454c2ff1c
HJ <hjk@gmail.com>
2020-09-26 22:27:39 +0900

    ADD makeBoard, mixBoard function

--- Bingo.cpp
+++ Bingo.cpp
@@ -1,7 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <time.h>
+#define SWAP(x, y, t) (t = x, x = y, y = t)
+
 using namespace std;
+
+void mixBoard(int A[][5]) {
+	int temp;
+	srand(time(NULL));
+
+	for (int i = 0; i < 100; i++) {
+		SWAP(A[rand() % 25][rand() % 25], A[rand() % 25][rand() % 25], temp);
+	}
+}
+
+void makeBoard(int A[][5]) {
+	int cnt = 1;
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			A[i][j] = cnt;
+			cnt++;
+		}
+	}
+
+	mixBoard(A);
+}
 
 int main() {
 
