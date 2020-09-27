500f389bf8eac9890fe7baf7619b831d90f3a8ac
HJ <hjk@gmail.com>
2020-09-28 00:42:38 +0900

    FEAT inputSize function

--- Bingo.cpp
+++ Bingo.cpp
@@ -1,8 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define N 10
 #include <stdio.h>
 
-int main() {
+int player[N][N], computer[N][N], size;
 
+void inputSize() {
+	while (1) {
+		printf("Enter the Board size(3 <= size <= 10): ");
+		scanf("%d", &size);
+		
+		if (2 < size && size < 11) {
+			return;
+		}
+	}
+}
+
+int main() {
+	inputSize();
 
 	return 0;
 }
