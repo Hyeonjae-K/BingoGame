3451d8f5af893b54e8642fb027f269e536fdb599
HJ <hjk@gmail.com>
2020-09-28 00:50:20 +0900

    FEAT makeBoard function

--- Bingo.cpp
+++ Bingo.cpp
@@ -1,8 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define SWAP(x, y, t) (t = x, x = y, y = t)
 #define N 10
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int player[N][N], computer[N][N], size;
+
+struct randomNums {
+	int a = rand() % size;
+	int b = rand() % size;
+	int c = rand() % size;
+	int d = rand() % size;
+};
 
 void inputSize() {
 	while (1) {
@@ -15,8 +25,30 @@ void inputSize() {
 	}
 }
 
+void makeBoard() {
+	int cnt = 1;
+
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			player[i][j] = cnt++;
+			computer[i][j] = cnt++;
+		}
+	}
+
+	for (int i = 0; i < size * 20; i++) {
+		struct randomNums A1, A2;
+		int temp;
+
+		SWAP(player[A1.a][A1.b], player[A1.c][A1.d], temp);
+		SWAP(player[A2.a][A2.b], player[A2.c][A2.d], temp);
+	}
+}
+
 int main() {
+	srand(time(NULL));
+
 	inputSize();
+	makeBoard();
 
 	return 0;
 }
