4d95909cae59cc07cbabfc48760ef539873108a2
HJ <hjk@gmail.com>
2020-09-29 00:16:19 +0900

    FEAT count bingo

--- Bingo.cpp
+++ Bingo.cpp
@@ -18,7 +18,7 @@ struct Random {
 struct Bingo {
 	int row = 0;
 	int column = 0;
-	int lefeCross = 0;
+	int leftCross = 0;
 	int rightCross = 0;
 	int bingo = 0;
 };
@@ -81,9 +81,11 @@ int main() {
 		swap(&player.B[pRand.a % size][pRand.b % size], &player.B[pRand.c % size][pRand.d % size]);
 		swap(&computer.B[cRand.a % size][cRand.b % size], &computer.B[cRand.c % size][cRand.d % size]);
 	}
-
-	int num;
+	
 	while (1) {
+		struct Bingo p, c;
+		int num;
+
 		system("cls");
 
 		for (int i = 0; i < size; i++) {
@@ -94,6 +96,7 @@ int main() {
 		}
 		printf("\n");
 
+		printf("Bingo: %d", p.bingo);
 		printf("Enter the number(Enter 0 to exit): ");
 		scanf("%d", &num);
 
@@ -127,8 +130,76 @@ int main() {
 				continue;
 			}
 			else {
-				struct Bingo P, C;
+				p.bingo = 0;
+				c.bingo = 0;
 
+				for (int i = 0; i < size; i++) {
+					p.row = 0;
+					p.column = 0;
+					c.row = 0;
+					c.column = 0;
+
+					for (int j = 0; j < size; j++) {
+						if (player.B[i][j] == 0) {
+							p.row++;
+						}
+						if (player.B[j][i] == 0) {
+							p.column++;
+						}
+						if (computer.B[i][j] == 0) {
+							c.row++;
+						}
+						if (computer.B[j][i] == 0) {
+							c.column++;
+						}
+					}
+
+					if (p.row == size) {
+						p.bingo++;
+					}
+					if (p.column == size) {
+						p.bingo++;
+					}
+					if (c.row == size) {
+						c.bingo++;
+					}
+					if (c.column == size) {
+						c.bingo++;
+					}
+				}
+
+				p.leftCross = 0;
+				p.rightCross = 0;
+				c.leftCross = 0;
+				c.rightCross = 0;
+
+				for (int i = 0; i < size; i++) {
+					if (player.B[i][i] == 0) {
+						p.leftCross++;
+					}
+					if (player.B[i][size - i - 1] == 0) {
+						p.rightCross++;
+					}
+					if (computer.B[i][i] == 0) {
+						c.leftCross++;
+					}
+					if (computer.B[i][size - i - 1] == 0) {
+						c.rightCross++;
+					}
+				}
+
+				if (p.leftCross == size) {
+					p.bingo++;
+				}
+				if (p.rightCross == size) {
+					p.bingo++;
+				}
+				if (c.leftCross == size) {
+					c.bingo++;
+				}
+				if (c.rightCross == size) {
+					c.bingo++;
+				}
 			}
 		}
 	}
