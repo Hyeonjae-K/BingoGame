4144ef2777825927349b1a41ebfd80c92121c745
HJ <hjk@gmail.com>
2020-09-28 23:25:44 +0900

    FEAT make board and mix

--- Bingo.cpp
+++ Bingo.cpp
@@ -2,8 +2,30 @@
 #define N 100
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+struct board {
+	int board[N][N] = { 0 };
+};
+
+struct random {
+	int a = rand();
+	int b = rand();
+	int c = rand();
+	int d = rand();
+};
+
+void swap(int* a, int* b) {
+	int t;
+	t = *a;
+	*a = *b;
+	*b = t;
+}
 
 int main() {
+	srand(time(NULL));
+
+	struct board player, computer;
 	int size, level;
 
 	while (1) {
@@ -24,6 +46,22 @@ int main() {
 		if (0 <= level && level <= 2) {
 			break;
 		}
+	}
+
+	int cnt = 1;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			player.board[i][j] = cnt;
+			computer.board[i][j] = cnt;
+			cnt++;
+		}
+	}
+
+	for (int i = 0; i < size * 20; i++) {
+		struct random pRand, cRand;
+
+		swap(&player.board[pRand.a % size][pRand.b % size], &player.board[pRand.c % size][pRand.d % size]);
+		swap(&computer.board[cRand.a % size][cRand.b % size], &computer.board[cRand.c % size][cRand.d % size]);
 	}
 
 	return 0;
