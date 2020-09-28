486529bfa591c8031b92a3d091fbdf4c95650282
HJ <hjk@gmail.com>
2020-09-28 23:13:44 +0900

    FEAT input size, level

--- Bingo.cpp
+++ Bingo.cpp
@@ -1,8 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define N 100
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+	int size, level;
 
+	while (1) {
+		system("cls");
+		printf("Enter the size(3 <= size <= 10): ");
+		scanf("%d", &size);
+
+		if (3 <= size && size <= 10) {
+			break;
+		}
+	}
+
+	while (1) {
+		system("cls");
+		printf("Enter the level(0: EASY, 1: NORMAL, 2: HARD): ");
+		scanf("%d", &level);
+
+		if (0 <= level && level <= 2) {
+			break;
+		}
+	}
 
 	return 0;
 }
